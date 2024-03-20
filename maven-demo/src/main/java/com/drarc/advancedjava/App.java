fc01a08b6027a43a7a7da52a7c53c4e4ba0cf8e1
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-20 21:32:51 +0530

    Demonstrate getting value from a config.properies file

--- maven-demo/src/main/java/com/drarc/advancedjava/App.java
+++ maven-demo/src/main/java/com/drarc/advancedjava/App.java
@@ -1,11 +1,23 @@
 package com.drarc.advancedjava;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Properties properties = new Properties();
+		String propertiesConfigPath = "/config.properties";
+		try {
+			properties.load(App.class.getResourceAsStream(propertiesConfigPath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String name = properties.getProperty("name");
+		System.out.println("Name : " + name);
 	}
 }
