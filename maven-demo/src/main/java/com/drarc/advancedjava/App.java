f2ca7ed3f23673ae4d885d57ace70f0d1f7264d0
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-23 22:44:19 +0530

    Added relevant comments for understanding

--- maven-demo/src/main/java/com/drarc/advancedjava/App.java
+++ maven-demo/src/main/java/com/drarc/advancedjava/App.java
@@ -4,81 +4,87 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Properties;
 
-
 /**
- * Hello world!
- *
+ * This class demonstrates various Java concepts such as custom annotations, reflection, and property file reading.
  */
 
+// Custom annotation to mark a class as configurable
 @Configurable(config = "Advanced Java")
 class MyClass {
-	int score = 0;
+    int score = 0; // Instance variable
 
-	@Field(description = "Name")
-	static String name  = "Ayush Jaipuriar";
+    // Custom annotation to describe a field
+    @Field(description = "Name")
+    static String name = "Ayush Jaipuriar"; // Static variable
 
-	private void print() {
-		System.out.println("printed something from private method : print");
-	}
+    // Private method to demonstrate reflection
+    private void print() {
+        System.out.println("printed something from private method : print");
+    }
 
-	public int sum(int a, int b) {
-		return a + b;
-	}
+    // Public method to demonstrate reflection
+    public int sum(int a, int b) {
+        return a + b;
+    }
 
-	public static double multiply(double a, double b) {
-		return a * b;
-	}
+    // Public static method to demonstrate reflection
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
 }
 
 public class App {
-	public static void main(String[] args) {
-		// Getting Data From a Properties File
-		Properties properties = new Properties();
-		String propertiesConfigPath = "/config.properties";
-		try {
-			properties.load(App.class.getResourceAsStream(propertiesConfigPath));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String name = properties.getProperty("name");
-		System.out.println("Name : " + name);
+    public static void main(String[] args) {
+        // Load properties from a file
+        Properties properties = new Properties();
+        String propertiesConfigPath = "/config.properties";
+        try {
+            properties.load(App.class.getResourceAsStream(propertiesConfigPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String name = properties.getProperty("name");
+        System.out.println("Name : " + name);
 
-		String env = System.getProperty("env");
-		System.out.println(env);
+        // Get system property
+        String env = System.getProperty("env");
+        System.out.println(env);
 
-		// Reflection API Usecases
-		try {
-			Class<?> reflectedClass = Class.forName("com.drarc.advancedjava.MyClass");
-			Object reflectedClassObj = reflectedClass.getDeclaredConstructor().newInstance();
-			// Private method with no parameter and a void return type
-			Method printMethod = reflectedClass.getDeclaredMethod("print");
-			printMethod.setAccessible(true);
-			printMethod.invoke(reflectedClassObj);
+        // Demonstrate reflection API use cases
+        try {
+            // Reflect the MyClass class
+            Class<?> reflectedClass = Class.forName("com.drarc.advancedjava.MyClass");
+            Object reflectedClassObj = reflectedClass.getDeclaredConstructor().newInstance();
 
-			// Public method with 2 parameters and an int return type
-			Method sumMethod = reflectedClass.getDeclaredMethod("sum", int.class, int.class);
-			int sum = (int) sumMethod.invoke(reflectedClassObj, 1, 2);
-			System.out.println(sum);
+            // Access and invoke a private method
+            Method printMethod = reflectedClass.getDeclaredMethod("print");
+            printMethod.setAccessible(true);
+            printMethod.invoke(reflectedClassObj);
 
-			// Public static method with 2 double parameters and a double return type
-			Method multiplyMethod = reflectedClass.getDeclaredMethod("multiply", double.class, double.class);
-			double product = (double) multiplyMethod.invoke(null, 6.0, 9.0);
-			System.out.println(product);
+            // Access and invoke a public method
+            Method sumMethod = reflectedClass.getDeclaredMethod("sum", int.class, int.class);
+            int sum = (int) sumMethod.invoke(reflectedClassObj, 1, 2);
+            System.out.println(sum);
 
-			// Using Reflection to get the custom annotation 'Field' property
-			java.lang.reflect.Field field = reflectedClass.getDeclaredField("name");
-			Field fieldAnnotation = field.getAnnotation(Field.class);
-			if (fieldAnnotation != null) {
-				System.out.println("Field Annotation Description = " + fieldAnnotation.description());
-			}
-			
-			Configurable annotation = reflectedClass.getAnnotation(Configurable.class);
-			if (annotation != null) {
-				System.out.println("Class annotation : " + annotation.toString());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            // Access and invoke a public static method
+            Method multiplyMethod = reflectedClass.getDeclaredMethod("multiply", double.class, double.class);
+            double product = (double) multiplyMethod.invoke(null, 6.0, 9.0);
+            System.out.println(product);
 
-	}
+            // Access and read a custom annotation on a field
+            java.lang.reflect.Field field = reflectedClass.getDeclaredField("name");
+            Field fieldAnnotation = field.getAnnotation(Field.class);
+            if (fieldAnnotation != null) {
+                System.out.println("Field Annotation Description = " + fieldAnnotation.description());
+            }
+            
+            // Access and read a custom annotation on a class
+            Configurable annotation = reflectedClass.getAnnotation(Configurable.class);
+            if (annotation != null) {
+                System.out.println("Class annotation : " + annotation.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
